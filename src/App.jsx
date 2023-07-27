648b56b86d984b81e1e075ff2ffdef1b35ef8b55
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-26 21:10:29 -0300

    mostrar el spinner cuando usamos el boton obtener frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 import { unstable_renderSubtreeIntoContainer } from "react-dom";
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     consultarApi();
   }, []);
   const consultarApi = async () => {
     try {
+      setMostrarSpinner(true);
       const respuesta = await fetch(
         "https://thesimpsonsquoteapi.glitch.me/quotes"
       );
       const dato = await respuesta.json();
       console.log(dato[0]);
       setPersonaje(dato[0]);
+      setMostrarSpinner(false);
     } catch (error) {
       console.log(error);
       // dejar un msj intuitivo para el usuario
@@ -28,7 +31,15 @@ function App() {
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase datoPersonaje={personaje}></Frase>
+        {mostrarSpinner === true ? (
+          <div className="my-5">
+            {" "}
+            <Spinner animation="border" variant="light" />
+          </div>
+        ) : (
+          <Frase datoPersonaje={personaje}></Frase>
+        )}
+
         <Button variant="warning" onClick={consultarApi}>
           Obtener frase
         </Button>
